af8fe278446c4aeb2b77ac4e1115e0dfadcecba7
Aqib Akram <aqibakram206@gmail.com>
2023-12-05 10:55:24 +0500

    nothing-to commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
+import Transition from "@/components/Transition";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />;
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
 }
+
+export default MyApp;
---
+++ pages/services/index.js
@@ -0,0 +1,53 @@
+import ServiceSlider from "@/components/ServiceSlider";
+import Bulb from "@/components/Bulb";
+import Circles from "@/components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variants";
+
+const Services = () => {
+  return (
+    <div className="h-full bg-primary/38 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              className="h2 xl:mt-8"
+            >
+              My Skills <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              I have hands-on experience in building and maintaining full-stack
+              web applications using Mern Stack.Specializing in Wordpress in
+              custom theme development,plugin integration, and site
+              optimization.As a Local SEO expert,I drive targeted visibility by
+              implementing effective strategies, optimizing Google My Business,
+              and leveraging local keywords for enhanced online presence.
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full lg:w-[60%] xl:max-w[65%]"
+          >
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default Services;
